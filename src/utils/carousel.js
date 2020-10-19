6f179a1727730f5beaf6ad99b2de7cd40503aa44
AnnaMarandola <annamarandola@gmail.com>
2020-10-19 13:24:51 +0200

    creating the profile with react-hook-form

--- src/utils/carousel.js
+++ src/utils/carousel.js
@@ -7,8 +7,19 @@ const CarouselWidget = () => {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="/images/SLIDE-1.jpg"
-          alt="first slide"
+          src="/images/slide1.png"
+          alt="slide1"
+        />
+        <Carousel.Caption>
+          <h3>Les meilleures ressources</h3>
+          <p>Découvrez et partagez !  </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="/images/slide2.jpg"
+          alt="second slide"
         />
         <Carousel.Caption>
           <h3>first slide</h3>
@@ -18,11 +29,22 @@ const CarouselWidget = () => {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="/images/SLIDE-2.jpg"
+          src="/images/slide3.jpg"
           alt="second slide"
         />
         <Carousel.Caption>
-          <h3>first slide</h3>
+          <h3>3rd slide</h3>
+          <p>salut salut </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="/images/slide4.jpg"
+          alt="second slide"
+        />
+        <Carousel.Caption>
+          <h3>4th slide</h3>
           <p>salut salut </p>
         </Carousel.Caption>
       </Carousel.Item>
