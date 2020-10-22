83056a9e2c4d628cd3e375eb8aa45f9217735938
AnnaMarandola <annamarandola@gmail.com>
2020-10-22 18:53:26 +0200

    edit reviews

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,4 +1,9 @@
-import { AUTH_USER, LOGOUT_USER, ADD_REVIEW, CLEAR_REVIEW } from "../types";
+import { 
+  AUTH_USER, 
+  LOGOUT_USER, 
+  ADD_REVIEW, 
+  CLEAR_REVIEW, 
+  GET_REVIEWS } from "../types";
 import * as api from "../../api";
 
 export const registerUser = (userData) => ({
@@ -35,3 +40,14 @@ export const clearReview = () => ({
   type: CLEAR_REVIEW,
   payload: null
 })
+
+export const getReviews = (limit) => ({
+  type: GET_REVIEWS,
+  payload: api.getReviews(limit)
+})
+
+export const loadMoreReviews = (limit, reviews) => ({
+  type: GET_REVIEWS,
+  payload: api.loadMoreReviews(limit, reviews)
+})
+
--- src/store/reducers/reviews.js
+++ src/store/reducers/reviews.js
@@ -1,12 +1,14 @@
-import { ADD_REVIEW, CLEAR_REVIEW} from '../types';
+import { ADD_REVIEW, CLEAR_REVIEW, GET_REVIEWS } from "../types";
 
-export default function (state = {}, action){
-    switch(action.type){
-        case ADD_REVIEW:
-            return {...state, addedReview: action.payload}
-        case CLEAR_REVIEW:
-            return { addedReview: action.payload}
-        default:
-            return state;
-    }
+export default function (state = {}, action) {
+  switch (action.type) {
+    case ADD_REVIEW:
+      return { ...state, addedReview: action.payload };
+    case CLEAR_REVIEW:
+      return { addedReview: action.payload };
+    case GET_REVIEWS:
+      return { ...state, adminReviews: action.payload };
+    default:
+      return state;
+  }
 }
